920ecb0c881d46195a1330c4a475bad37da311c2
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-01-08 17:02:04 -0500

    wire up error and another page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import App from './App'
+import ContactPage from './pages/ContactPage';
+import Root from "./routes/root";
+
 import './index.css'
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={createBrowserRouter([
+      {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "contacts/:contactId",
+        element: <ContactPage />,
+      },
+    ])} />
   </React.StrictMode>,
 )
