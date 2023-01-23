824c3230661503dfcb809ec7f48809f42c5f4722
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-01-22 23:21:33 -0500

    Create useListenForSave for saving form

---
+++ client/src/pages/taskPage/useListenForSave.ts
@@ -0,0 +1,19 @@
+import useEventListener from "./useEventListener";
+
+const useListenForSave = (formName:string) => {
+    let isControlPressed = false;
+    useEventListener('keydown', ({ key }: any) => {
+        if (key === 'Control') {
+            isControlPressed = true;
+        } else {
+            if (isControlPressed && key === 's') {
+                document.getElementById(formName)
+                    ?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
+            }
+            isControlPressed = false;
+        }
+
+    });
+};
+
+export default useListenForSave;
