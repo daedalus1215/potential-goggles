0514bb7377a67fe0778aa4c9102b16de7b7c38ec
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-04 21:39:51 -0500

    Migrate updateContactForm

--- client/src/actions.ts
+++ client/src/actions.ts
@@ -1,9 +1,17 @@
 import { redirect } from "react-router-dom";
-import { deleteContact, Params } from "./contacts";
+import { deleteContact, Params, updateContact } from "./contacts";
+import { ActionInterface } from "./pages/interfaces";
 
 
-export async function destroy({ params }: Params) {
+export async function destroyContact({ params }: Params) {
     // throw new Error("oh dang!");
     deleteContact(params.contactId);
     return redirect("/");
 }
+
+export const updateContactForm: ActionInterface = async ({ request, params }) => {
+    let formData = await request.formData();
+    return updateContact(params.contactId, {
+        favorite: formData.get('favorite') === 'true'
+    });
+};
