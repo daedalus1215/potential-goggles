649b1d12323c9ddb0600810db7447e687975a35c
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-06 10:51:12 -0500

    Bring in more utils

---
+++ client/src/utils/index.tsx
@@ -0,0 +1,5 @@
+export { default as getCurrentDateTimeEstFormat } from './formatters/getCurrentDateTimeEstFormat';
+export { default as displayMsInFractionalHourFormat } from './formatters/displayMsInFractionalHourFormat';
+export { default as formatMinsAndSecsForDisplay } from './formatters/formatMinsAndSecsForDisplay';
+export { default as fetchApiData } from './fetchApiData';
+export { default as selectNormalizer } from './normalizers/selectNormalizer';
