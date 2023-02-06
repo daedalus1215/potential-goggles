561943bdb3d51c45e2234e4b4553b91f461d1208
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-06 10:47:13 -0500

    Carry over more from timer

---
+++ client/src/pages/taskPage/timer/Timer.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-useless-computed-key */
+import React, { useState, useCallback, useEffect } from 'react';
+import ms from 'pretty-ms';
+import useUpdateCurrentTime from './hooks/useUpdateCurrentTime';
+import TimerButtons from './timerButtons/TimerButtons';
+import styles from './Timer.module.css';
+
+const Timer = ({ task }) => {
+  const [isActive, setIsActive] = useState(false);
+  const [time, setTime] = useState(task.time);
+  const setTimeCallback = useCallback(times => setTime(times), [setTime]);
+  useUpdateCurrentTime(time, isActive, setTimeCallback);
+  const msTime = (time && ms(time, { secondsDecimalDigits: 2 })) || 0;
+
+  const original = (task?.time && ms(task.time, { secondsDecimalDigits: 2 })) || 0
+  return <TimerButtons
+    time={time}
+    setTime={setTimeCallback}
+    isActive={isActive}
+    setIsActive={setIsActive}
+  >
+    <div data-test-id="timer__display__content">
+      {/* <div>{task.time}</div> */}
+      <div data-test-id="fractionHour">{`Hours: ${original}`}</div>
+      <input
+        data-test-id="secondDecimalDigitHour"
+        className={styles.input}
+        value={msTime}
+        readOnly
+      />
+    </div>
+  </TimerButtons>
+};
+
+export default Timer;
