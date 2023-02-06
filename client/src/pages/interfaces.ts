187d08779431ff3afdeed41c47719971f5d3bb6b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-06 10:46:31 -0500

    Interfaces into interfaces

--- client/src/pages/interfaces.ts
+++ client/src/pages/interfaces.ts
@@ -3,3 +3,37 @@ export type ErrorInterface = {
     statusText: string;
     message: string;
 }
+export interface Contact {
+    id: string;
+    first: string;
+    last: string;
+    avatar: string;
+    twitter: string;
+    notes: string;
+    favorite: boolean;
+};
+export interface Task {
+    _id: string;
+    title?: string;
+    description: string;
+    projectId: number;
+    time: number;
+    favorite?: boolean;
+}
+export interface Params {
+    params: {
+        taskId: string
+    };
+}
+/**
+ * https://dev.to/iamandrewluca/typed-fetch-response-in-typescript-1eh1
+ */
+export interface TypedResponse<T = any> extends Response {
+    /**
+     * this will override `json` method from `Body` that is extended by `Response`
+     * interface Body {
+     *     json(): Promise<any>;
+     * }
+     */
+    json<P = T>(): Promise<P>
+}
