4f70b7a19c13aa44fad12e7e588f094e29145842
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-07 20:46:48 -0500

    Refactor DateTimeButton to take in task

--- client/src/pages/taskPage/dateTimePage/DateTimeButton.tsx
+++ client/src/pages/taskPage/dateTimePage/DateTimeButton.tsx
@@ -4,20 +4,22 @@ import cn from 'classnames';
 import { Modal, Button } from '../../../components';
 import DateTimePage from './page/DateTimePage';
 import styles from './DateTimeButton.module.css';
+import { Task } from '../../interfaces';
+
 interface DateTimeButtonProp {
-  taskId: string;
+  task: Task;
 }
 
-const DateTimeButton: React.FC<DateTimeButtonProp> = ({ taskId }) => {
+const DateTimeButton: React.FC<DateTimeButtonProp> = ({ task }) => {
   const [isShowing, setIsShowing] = useState(false);
 
   return (
     <div className={cn(styles.outline)}>
-      <Button className={cn(styles.dateTimeButton, 'glyphicon glyphicon-time')} onClick={() => setIsShowing(!isShowing)} />
+      <Button className={cn(styles.dateTimeButton, 'bi bi-clock')} onClick={() => setIsShowing(!isShowing)} />
 
       {isShowing ? (
         <Modal setIsShowing={setIsShowing}>
-          <DateTimePage taskId={taskId} setIsShowing={setIsShowing} />
+          <DateTimePage task={task} setIsShowing={setIsShowing} />
         </Modal>
       ) : (
         []
