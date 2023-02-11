b86545936c038cbc7701939933dffb415f4d7630
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-10 21:55:45 -0500

    Clean up contacts/index

--- client/src/contacts/index.ts
+++ client/src/contacts/index.ts
@@ -1,27 +1,5 @@
-import { Contact, Task, TypedResponse } from "../pages/interfaces";
+import { Task, TypedResponse } from "../pages/interfaces";
 import fetchApiData from "../utils/fetchApiData";
-
-let data: Contact[] = [{
-    id: 'dsd',
-    first: "Jeremy",
-    last: "Holmes",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "@jHolmes",
-    notes: "Some notes",
-    favorite: true,
-},
-{
-    id: 'd2sd',
-    first: "Ysdsour",
-    last: "Namsdase",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "twitter handle",
-    notes: "Some notes",
-    favorite: true,
-}];
-
-
-export const getContacts = (): Contact[] => data;
 
 export const getContactsSearch = async (name?: string): Promise<Task[]> => {
     const results = await fetch('http://localhost:3001/api/tasks') as TypedResponse<Task[]>;
@@ -32,30 +10,7 @@ export const getContactsSearch = async (name?: string): Promise<Task[]> => {
         return tasks;
     } else {
         console.log('name', name);
-
         return tasks.filter((task: Task) => task.description.toLowerCase().includes(name.toLowerCase()));
     }
-
-}
-export const createContact = () => {
-    data.push({
-        id: 'd2sdw',
-        first: "Yddour",
-        last: "Naddsme",
-        avatar: "https://placekitten.com/g/200/200",
-        twitter: "your_handle",
-        notes: "Some notes",
-        favorite: true,
-    });
-};
-
-export const deleteContact = (id: string) => {
-    data = data.filter(contact => contact.id !== id);
-}
-
-export const getContact = async (index: string): Promise<Task> => {
-    const task = await fetchApiData<Task>(`http://localhost:3001/api/task/${index}`, {});
-    console.log('task', task)
-    return task;
 }
 
