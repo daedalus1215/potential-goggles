d694339a4bec502df8e88119f678149497f6464a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-10 21:58:59 -0500

    Adjust updateDateTimeAction and remove redux elements

--- server/application/requestHandlers/tasks/updateDateTimeAction.js
+++ server/application/requestHandlers/tasks/updateDateTimeAction.js
@@ -1,16 +1,8 @@
-const apiResponse = require('../apiResponse');
 const TaskService = require('../../../domain/services/tasks/TaskService');
-const { UPDATE_DATE_TIME_RESPONSE } = require('../reduxTypes');
-
-
-
 
 module.exports = (req, res) => {
-    const responder = apiResponse(res, UPDATE_DATE_TIME_RESPONSE);
-
     const id = req.params.taskId;
     const dateTime = req.body;
-
     const updatedTaskWithDateTime = TaskService.updateDateTimeOfTask(id, dateTime);
-    responder(updatedTaskWithDateTime);
+    res.jsonp(updatedTaskWithDateTime);
 };
