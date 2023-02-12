7b8af00c6c9943571f0feabd6c1d6fe80299fa48
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-12 02:29:13 -0500

    Refactoring slightly the SaveButton

--- client/src/components/saveButton/SaveButton.tsx
+++ client/src/components/saveButton/SaveButton.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import cn from 'classnames';
-import styles from './SaveButton.module.css';
 import Button from '../button/Button';
 
-interface props {
-    name: string;
-}
-const SaveButton: React.FC<props> = ({name}) => <Button
+import styles from './SaveButton.module.css';
+
+const SaveButton: React.FC = () => <Button
     type="submit"
-    className={cn(styles.submit, "glyphicon glyphicon-floppy-save")}
-    onClick={() => {
-        document.getElementById(name)
-            ?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
-    }}
+    className={styles.submit}
+    // className={cn(styles.submit, "glyphicon glyphicon-floppy-save")}
+    // onClick={() => {
+    //     document.getElementById(name)
+    //         ?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
+    // }}
+    value="Save Form"
 />
 
 export default SaveButton;
