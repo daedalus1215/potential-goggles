8d2449c41978017b75026595ac26d9eb44c10ff1
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-14 08:40:44 -0500

    Add more strict typing and add FetchAllTaskTitles to TaskService

--- server/domain/services/tasks/TaskService.ts
+++ server/domain/services/tasks/TaskService.ts
@@ -1,15 +1,21 @@
-import { DateTimeDto } from "../../../dist/interfaces";
+import { DateTimeDto, Task } from "../../../interfaces";
+import { TaskTitle } from "../../../interfaces";
 
 const FetchAllTasksRepository = require('../../../infrastructure/repositories/tasks/Repositories/FetchAllTasksRepository');
 const FetchTaskByIdRepository = require('../../../infrastructure/repositories/tasks/Repositories/FetchTaskByIdRepository/FetchTaskByIdRepository');
 const UpdateDateTimeRepository = require('../../../infrastructure/repositories/tasks/Repositories/UpdateDateTimeRepository');
+const FetchAllTaskTitlesRepository = require('../../../infrastructure/repositories/tasks/Repositories/FetchAllTaskTitlesRepository');
 const updateTask = require('../../../infrastructure/repositories/tasks/Repositories/updateTask/updateTask');
 
-const TaskService = {
+export const TaskService = {
   fetchAllTasks: () => FetchAllTasksRepository(),
-  fetchTaskById: (taskId:any) => FetchTaskByIdRepository(taskId),
-  updateTask: (dto:any, res:any) => updateTask(dto, res),
+  fetchTaskById: (taskId: string) => FetchTaskByIdRepository(taskId),
+  updateTask: (dto: any, res: any) => updateTask(dto, res),
   updateDateTimeOfTask: (taskId: string, dateTime: DateTimeDto) => UpdateDateTimeRepository(taskId, dateTime),
+  fetchAllTaskTitles: () => FetchAllTaskTitlesRepository(),
 };
+
+
+export default TaskService;
 
 module.exports = TaskService;
