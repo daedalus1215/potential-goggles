aaa0a0d97fed2dc67485901055597ae619686ce6
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-19 21:21:15 -0500

    tasks loader

--- client/src/actions/loaders.ts
+++ client/src/actions/loaders.ts
@@ -1,11 +1,11 @@
-import { fetchTask } from './actions'
+import { fetchTask, fetchTasks } from './actions'
 import type { LoaderFunctionArgs } from "@remix-run/router";
 import { Task, TypedResponse } from '../interfaces';
 
 export const searchLoader = async ({ request, params }: LoaderFunctionArgs) => {
     const url = new URL(request.url)
     const q = url.searchParams.get('q') as string
-    const results = (await fetch('http://localhost:3001/api/tasks')) as TypedResponse<Task[]>
+    const results = (await fetch('http://localhost:3001/api/tasks-titles')) as TypedResponse<Task[]>
     const tasks = await selectSearchResult(results, q);
     const selectedId = params.id;
 
@@ -25,6 +25,12 @@ const selectSearchResult = async (results: TypedResponse<Task[]>, name?: string)
 export const taskLoader = async ({ params }: LoaderFunctionArgs) => {
     const task = await fetchTask(params?.taskId ?? '')
     return task
+}
+
+
+export const tasksLoader = async ({ params }: LoaderFunctionArgs) => {
+    const tasks = await fetchTasks()
+    return tasks
 }
 
 export const dateTimeLoader = async ({ params }: LoaderFunctionArgs) => {
