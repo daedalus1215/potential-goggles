30094cc3c9d504887f53659633d7c760799ed4ec
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-23 21:42:28 -0500

    Refactor UploadButton to use IconButton

--- client/src/pages/indexPage/UploadButton/UploadButton.tsx
+++ client/src/pages/indexPage/UploadButton/UploadButton.tsx
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
-import Button from '../../../components/button/Button';
-import cn from 'classnames';
+import IconButton from '../../../components/iconButton/IconButton';
 import UploadModal from './UploadModal';
-
-import styles from './UploadButton.module.css';
-import classNames from 'classnames';
 
 const UploadButton = () => {
     const [isModalShowing, setIsModalShowing] = useState(false);
 
     return (<>
-        <Button
-            className={cn(styles.uploadButton)}
-            onClick={setIsModalShowing}>
-            <div className={styles.wrap}>
-                <i className={classNames("bi bi-cloud-upload", styles.icon)}></i>
-                <span className={styles.title}>Upload File</span>
-            </div>
-        </Button>
+        <IconButton
+            onClick={setIsModalShowing}
+            icon="bi bi-cloud-upload"
+            title="Upload"
+        />
         <UploadModal
             isShowing={isModalShowing}
             setIsShowing={setIsModalShowing}
