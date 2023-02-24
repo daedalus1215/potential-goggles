8ab873d2aa449a791359d418263f870ece9cafce
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-23 21:42:14 -0500

    Refactor DownloadButton to use IconButton

--- client/src/pages/indexPage/DownloadButton/DownloadButton.tsx
+++ client/src/pages/indexPage/DownloadButton/DownloadButton.tsx
@@ -1,35 +1,22 @@
 import React from 'react';
-import cn from 'classnames';
-import { Button } from '../../../components';
 import { fetchTasks } from '../../../actions/actions';
+import IconButton from '../../../components/iconButton/IconButton';
 import writeJsonFile from './writeJsonFile';
-
-import styles from './DownloadButton.module.css';
-import classNames from 'classnames';
 
 const DownloadButton: React.FC = (...tasks) => {
   const handleDownload = async () => {
     const tasks = await fetchTasks();
-    console.log(tasks);
-
-    console.log('assembledTasked', tasks)
     return writeJsonFile(tasks)
   }
 
   return (
     <>
       {tasks?.length && (
-        <Button
-          data-testid="btn-download"
-          title="Download Tasks"
-          type="a"
-          className={cn(styles.buttonDownload, 'glyphicon glyphicon-download-alt')}
-          onClick={handleDownload}>
-          <div className={styles.wrap}>
-            <i className={classNames("bi bi-cloud-download", styles.icon)}></i>
-            <span className={styles.title}>Download</span>
-          </div>
-        </Button>
+        <IconButton 
+          onClick={handleDownload}
+          icon="bi bi-cloud-download"
+          title="Download"
+        />        
       )}
     </>
   );
