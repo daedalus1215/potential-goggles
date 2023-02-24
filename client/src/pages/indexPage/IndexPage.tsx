2b5c08fa43862c55b853654254612634335bd019
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-23 21:42:54 -0500

    Add TagButton to the IndexPage

--- client/src/pages/indexPage/IndexPage.tsx
+++ client/src/pages/indexPage/IndexPage.tsx
@@ -2,15 +2,17 @@ import DownloadButton from './DownloadButton/DownloadButton';
 import UploadButton from './UploadButton/UploadButton';
 
 import styles from './IndexPage.module.css';
+import TagButton from '../../components/tagButton/TagButton';
 
-export default function IndexPage() {
+export default function IndexPage () {
 
   return (
     <p id="zero-state">
       <br />
+      <TagButton />
+      <div className={styles.underline}></div>
       <UploadButton />
       <DownloadButton />
-      <div className={styles.underline}></div>
     </p>
   )
 }
