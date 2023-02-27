bc3bb653c17200c55d932e13e7816fdb75cd549b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-02-26 21:30:30 -0500

    Allow Tag Item route to edit tag pa ge

--- client/src/pages/tagPage/tagItem/TagItem.tsx
+++ client/src/pages/tagPage/tagItem/TagItem.tsx
@@ -1,15 +1,26 @@
+import useRippleEffectById from '@/components/button/useRippleEffect/useRippleEffectById';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './TagItem.module.css';
 
 interface props {
-    _id:string;
-    name:string;
+    _id: string;
+    name: string;
 }
-const TagItem:React.FC<props> = ({_id, name}) => {
-    return (<div className={styles.content}>
+
+const TagItem: React.FC<props> = ({ _id, name }) => {
+    const rippleClick = useRippleEffectById(_id, () => {});
+
+    return <Link
+        to={`/tag/${_id}`}
+        id={_id}
+        className={styles.content}
+        key={_id}
+        onClick={rippleClick}
+        data-testid="DateTimeItem">
         <span className={styles.name}>{name}</span>
-    </div>);
+    </Link>
 }
 
 export default TagItem;
