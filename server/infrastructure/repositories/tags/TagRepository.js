d6ad0160dcc20f435ade8eb1c4e67ca6e1126bf3
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-01 08:30:15 -0500

    Refactor TagRepo to not use res

--- server/infrastructure/repositories/tags/TagRepository.js
+++ server/infrastructure/repositories/tags/TagRepository.js
@@ -1,17 +1,16 @@
 const Tag = require('../../models/Tag');
-const hydrateAndResponse = require('../../hydrators/hydrateAndResponse');
 
 const TagRepository = {
   deleteTag: (id) => Tag.deleteOne({ _id: id }),
   fetchTagById: async (id, res) => await Tag.findById(id),
-
-  updateTag: async (dto, res) => {
+  updateTag: async (dto) => {
     const tag = await Tag.findById(dto._id);
 
     tag.description = dto.description;
     tag.name = dto.name;
 
-    tag.save(hydrateAndResponse(res));
+    const savedTag = await tag.save();
+    return savedTag;
   }
 };
 
