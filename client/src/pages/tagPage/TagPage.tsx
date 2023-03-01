40f540807a51ea0e4c0279322a9786b6663d9cdf
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-01 08:27:41 -0500

    Add the add button and form to TagPage

--- client/src/pages/tagPage/TagPage.tsx
+++ client/src/pages/tagPage/TagPage.tsx
@@ -1,25 +1,35 @@
-import  React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { Tag as TagInterface } from '../../interfaces';
+import AddButton from '@/components/addButton/AddButton';
+import React from 'react';
+import { Form, useLoaderData } from 'react-router-dom';
+import { Tag as TagInterface } from '@/interfaces';
 import Tag from './tagItem/TagItem';
 
 import styles from './TagPage.module.css';
 
-const TagePage:React.FC = () => {
+const TagePage: React.FC = () => {
     const tags = useLoaderData() as TagInterface[];
 
     if (!tags) {
         throw new Response("", {
-          status: 404,
-          statusText: "Tags not found!",
+            status: 404,
+            statusText: "Tags not found!",
         });
-      }
+    }
 
-    return (<div className={styles.grid}>
-        {tags.map((tag:TagInterface) => {
-            return <Tag key={tag._id} _id={tag._id} name={tag.name}/>
-        })}
-    </div>);
+    return (
+        <div className={styles.page}>
+            <Form
+                method='post'>
+                <AddButton />
+
+                <div className={styles.grid}>
+                    {tags.map((tag: TagInterface) => {
+                        return <Tag key={tag._id} _id={tag._id} name={tag.name} />
+                    })}
+                </div>
+            </Form>
+        </div>
+    );
 }
 
 export default TagePage;
