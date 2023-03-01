24e721459329b45aaf8007af31c804e24a687f27
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-01 08:29:59 -0500

    Refactor the addTag repo func

--- server/infrastructure/repositories/tags/addTag.js
+++ server/infrastructure/repositories/tags/addTag.js
@@ -1,10 +1,10 @@
 const hydrateAndResponse = require("../../hydrators/hydrateAndResponse");
 const getTagModel = require("../../models/getTagModel");
 
-module.exports = (tagDto, res) => {
+module.exports = async (tagDto) => {
     const tag = getTagModel();
     tag.toObject();
     tag.description = tagDto.description;
     tag.name = tagDto.name;
-    tag.save(hydrateAndResponse(res));
+    return await tag.save();
 };
