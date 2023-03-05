b8153225080702519b4ceb03b879fcf927ae176a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-05 17:01:36 -0500

    Create a BackButton

---
+++ client/src/components/BackButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from './Button';
+
+type props = {
+    path: string;
+};
+
+const BackButton: React.FC<props> = ({ path }) => {
+    const navigate = useNavigate();
+    return <Button
+        onClick={() => {
+            navigate(path)
+        }}>
+        <i className="bi bi-backspace"></i>
+    </Button>
+}
+
+export default BackButton;
