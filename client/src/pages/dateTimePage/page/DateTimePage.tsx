354a199d1812a21dd467d47c95dba08f700f98b9
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-04 21:50:55 -0500

    Add title to DateTime and some styles

--- client/src/pages/dateTimePage/page/DateTimePage.tsx
+++ client/src/pages/dateTimePage/page/DateTimePage.tsx
@@ -4,6 +4,7 @@ import { getCurrentDateTimeEstFormat } from '@/utils';
 import { Task } from '@/interfaces';
 import DateTimeListView from '../dateTimeListView/DateTimeListView';
 import AddButton from '@/components/addButton/AddButton';
+import { TopBar } from '@/components';
 
 import styles from './DateTimePage.module.css';
 
@@ -19,15 +20,23 @@ const DateTimePage: React.FC = () => {
 
   // const millisecondsInFractionalHourFormat = displayMsInFractionalHourFormat(task.time);
   return (<div className={styles.page}>
-    <Form
-      // action={`/date-time/${task._id}`}
-      method='post'>
-      <input type="hidden" name="taskId" value={task._id} />
-      <input type="hidden" name="date" value={getCurrentDateTimeEstFormat()} />
-      <input type="hidden" name="minutes" value='00:00' />
-      <input type="hidden" name="id" value='' />
-      <AddButton />
-    </Form>
+    <div className={styles.header}>
+
+      <h2 className={styles.h2}>{task.title}</h2>
+      <TopBar>
+        <>
+          <Form
+            // action={`/date-time/${task._id}`}
+            method='post'>
+            <input type="hidden" name="taskId" value={task._id} />
+            <input type="hidden" name="date" value={getCurrentDateTimeEstFormat()} />
+            <input type="hidden" name="minutes" value='00:00' />
+            <input type="hidden" name="id" value='' />
+            <AddButton />
+          </Form>
+        </>
+      </TopBar>
+    </div>
     <div className={styles.childrenContent}>
       <DateTimeListView task={task} />
     </div>
