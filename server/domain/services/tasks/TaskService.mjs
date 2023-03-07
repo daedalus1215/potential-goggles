6ccc03a39123ad32f6f490d1f57765df4458ff87
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-06 21:00:23 -0500

    Fix Fetch All Tasks method in the TaskService

--- server/domain/services/tasks/TaskService.mjs
+++ server/domain/services/tasks/TaskService.mjs
@@ -1,11 +1,12 @@
 import CreateDateTimeRepository from "./taskRepository/dateTime/CreateDateTimeRepository.mjs";
 import UpdateDateTimeRepository from "./taskRepository/dateTime/UpdateDateTimeRepository.mjs";
+import FetchAllTasksRepository from "./taskRepository/FetchAllTasksRepository.mjs";
 import FetchAllTaskTitlesRepository from "./taskRepository/FetchAllTaskTitlesRepository.mjs";
 import FetchTaskByIdRepository from "./taskRepository/FetchTaskByIdRepository/FetchTaskByIdRepository.mjs";
 import updateTask from "./taskRepository/updateTask/updateTask.mjs";
 
 export const TaskService = {
-  fetchAllTasks: () => FetchAllTaskTitlesRepository(),
+  fetchAllTasks: () => FetchAllTasksRepository(),
   fetchTaskById: (taskId) => FetchTaskByIdRepository(taskId),
   updateTask: (dto) => updateTask(dto),
   updateDateTimeOfTask: (taskId, dateTime) => UpdateDateTimeRepository(taskId, dateTime),
