f0b7b8510a22989d82fd55d933c18c615a94d996
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-06 21:04:24 -0500

    Update FetchAllTaskTitlesRepository

--- server/domain/services/tasks/taskRepository/FetchAllTaskTitlesRepository.mjs
+++ server/domain/services/tasks/taskRepository/FetchAllTaskTitlesRepository.mjs
@@ -1,10 +1,12 @@
 import TaskModel from "../../../../infrastructure/models/TaskModel.mjs";
 
 const FetchAllTaskTitlesRepository = async () => {
-
     const tasks = await TaskModel.find();
-
-    return tasks.map((task) => ({ _id: task?._id ?? 'some id', title: task?.title ?? 'no title' }))
+    return tasks
+        .map((task) => ({
+            _id: task?._id ?? 'stubAnId',
+            title: task?.title ?? 'no title'
+        }))
 };
 
 
