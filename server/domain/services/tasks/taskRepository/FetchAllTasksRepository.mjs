3c2552c5c6d89923556c1f0b924b4930e25a3921
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-06 21:00:48 -0500

    Migrate FetchAllTasksRepository to mjs

--- server/domain/services/tasks/taskRepository/FetchAllTasksRepository.mjs
+++ server/domain/services/tasks/taskRepository/FetchAllTasksRepository.mjs
@@ -1,16 +1,3 @@
-import { Task } from "../../../../interfaces";
+import TaskModel from "../../../../infrastructure/models/TaskModel.mjs";
 
-const hydrate = require('../../../../infrastructure/hydrators/hydrate');
-const TaskModel = require('../../../../infrastructure/models/TaskModel');
-const EntityToDto = require('./FetchTaskByIdRepository/EntityToDto.mjs');
-
-export default () => TaskModel.find({}, hydrateTask).sort('-_id');
-
-const hydrateTask = (err: any, doc:any) => {
-    const tasks = hydrate(err, doc);
-    if (tasks.error) {
-        return tasks.error
-    }
-    const dtos = tasks.map((task: Task) => EntityToDto(task));
-    return dtos;
-}
+export default async () => TaskModel.find({}).sort('-_id');
