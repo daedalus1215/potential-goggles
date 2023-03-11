b09859b99704cca55087a6d6eb92e85dca31ff5f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-10 22:05:57 -0500

    Move new button into Index page

--- client/src/pages/indexPage/IndexPage.tsx
+++ client/src/pages/indexPage/IndexPage.tsx
@@ -3,13 +3,20 @@ import UploadButton from './UploadButton/UploadButton';
 
 import styles from './IndexPage.module.css';
 import TagButton from '../../components/tagButton/TagButton';
+import { Form } from 'react-router-dom';
 
-export default function IndexPage () {
+export default function IndexPage() {
 
   return (
     <p id="zero-state">
       <br />
       <TagButton />
+      <Form method="post" className={styles.newForm}>
+        <input type="hidden" name="formId" value="newTask" />
+        <button type="submit" className="button">
+          New
+        </button>
+      </Form>
       <div className={styles.underline}></div>
       <UploadButton />
       <DownloadButton />
