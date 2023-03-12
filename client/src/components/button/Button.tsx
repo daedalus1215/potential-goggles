17146e2bbbc0b260fa5ef6cd8f69d64e0c962198
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-11 19:50:56 -0500

    Expand Button comp to use category

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -4,6 +4,14 @@ import useRippleEffect from './useRippleEffect/useRippleEffect';
 
 import styles from './Button.module.css';
 
+export const Category = {
+  'primary': 'primary',
+  'primary2nd': 'primary2nd',
+  'danger': 'danger',
+  'info': 'info',
+  'accent': 'accent'
+} as const;
+
 interface ButtonProps {
   className?: string;
   onClick?: (e?: any) => void;
@@ -13,13 +21,36 @@ interface ButtonProps {
   title?: string;
   type?: string;
   disabled?: boolean
+  category?: keyof typeof Category
 }
 
-const Button: React.FC<ButtonProps> = ({ className, onClick, value, children, testid, title, type, disabled, ...rest }) => {
+const Button: React.FC<ButtonProps> = ({ className, onClick, value, children, testid, title, type, disabled, category, ...rest }) => {
   const clickCallback = useRippleEffect('button', onClick || (() => { }));
+  let categoryClass = {};
+  switch (category) {
+    case Category.info:
+      categoryClass = { [styles.info]: true }
+      break;
+    case Category.primary:
+      categoryClass = { [styles.primary]: true }
+      break;
+    case Category.primary2nd:
+      categoryClass = { [styles.primary2nd]: true }
+      break;
+    case Category.danger:
+      categoryClass = { [styles.danger]: true }
+      break;
+    case Category.accent:
+      categoryClass = { [styles.accent]: true }
+      break;
+    default:
+      categoryClass = {}
+      break;
+  }
+
   return (
     <button
-      className={cn(styles.baseBtn, className)}
+      className={cn(styles.baseBtn, className, categoryClass)}
       onClick={clickCallback}
       data-testid={testid}
       disabled={disabled}
