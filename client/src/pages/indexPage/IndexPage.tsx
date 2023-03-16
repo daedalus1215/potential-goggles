61d8760c2dda1509633a1ca5d37b5523e31b6f23
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-16 10:52:09 -0400

    Wire in new comp into IndexPage

--- client/src/pages/indexPage/IndexPage.tsx
+++ client/src/pages/indexPage/IndexPage.tsx
@@ -3,9 +3,12 @@ import UploadButton from './UploadButton/UploadButton';
 import TagButton from '../../components/tagButton/TagButton';
 
 import styles from './IndexPage.module.css';
+import { useLoaderData } from 'react-router-dom';
+import ActivityList from './ActivityList';
+import { TodaysActivity } from '@/interfaces';
 
 export default function IndexPage() {
-
+  const todaysActivities = useLoaderData() as TodaysActivity[];
   return (
     <p id="zero-state">
       <br />
@@ -13,6 +16,7 @@ export default function IndexPage() {
       <div className={styles.underline}></div>
       <UploadButton />
       <DownloadButton />
+      <ActivityList activities={todaysActivities}/>
     </p>
   )
 }
