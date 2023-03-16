3c68588e5b5ad8e26189ab09e0b1511f4bd0b7a0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-16 10:52:30 -0400

    Rename todays activity action

--- server/application/requestHandlers/tasks/dateTime/FetchTodaysTasksAction.mjs
+++ server/application/requestHandlers/tasks/dateTime/FetchTodaysTasksAction.mjs
@@ -1,10 +1,9 @@
 import FetchTodaysTasks from "../../../../domain/services/tasks/taskRepository/FetchTodaysTasks.mjs";
 
 
-export const FetchTodaysTasksAction = async (req, res) => {
-    console.log('yes')
+export const FetchTodaysActivity = async (req, res) => {
     const updatedTaskWithDateTime = await FetchTodaysTasks();
     res.jsonp(updatedTaskWithDateTime);
 };
 
-export default FetchTodaysTasksAction;
+export default FetchTodaysActivity;
