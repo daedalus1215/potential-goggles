e7f4dcf2c81ede9c0da53c4d5d462c968872e2c4
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-16 10:54:21 -0400

    Clean up FetchTodaysTasks and make it work

--- server/domain/services/tasks/taskRepository/FetchTodaysTasks.mjs
+++ server/domain/services/tasks/taskRepository/FetchTodaysTasks.mjs
@@ -1,27 +1,26 @@
 import TaskModel from "../../../../infrastructure/models/TaskModel.mjs";
 
+const getDate = (date) => {
+    return `${date.getUTCFullYear()}-0${date.getUTCMonth() + 1}-${date.getDate()}`;
+}
+
 export const FetchTodaysTasks = async () => {
-    const today = `${new Date().getUTCFullYear()}-0${new Date().getUTCMonth() + 1}-${new Date().getDate() - 1}`;
+    const today = getDate(new Date());
     console.log(today)
-    const tasks = await TaskModel.find({ '$where': `this.date.toJSON().slice(0, 10) >= "${'2023-03-14'}"` }).sort('-_id');
+    const tasks = await TaskModel.find({ '$where': `this.date.toJSON().slice(0, 10) == "${today}"` }).sort('-_id');
     return tasks
         .map((task) => ({
             _id: task?._id ?? 'stubAnId',
             title: task?.title ?? 'no title',
+            date: task.date,
             totalTimeToday: task.time
-                ?.filter(t1 => t1?.date)
-                .filter(ti => `${ti.date.getUTCFullYear()}-0${ti.date.getUTCMonth() + 1}-${ti.date.getDate() - 1}` == today)
+                .filter(t1 => t1?.date)
+                .filter(t1 => getDate(t1.date) == today)
                 .map(t1 => t1.time)
-                .reduce((t1, t2) => {
-                    console.log('t1', t1);
-                    console.log('t2', t2);
-                    const time1 = t1
-                    const time2 = t2
-                    return time1 + time2;
-                }, 0),    
+                .reduce((t1, t2) => t1 + t2, 0),
             times: task.time
                 .filter(t1 => t1?.date)
-                .filter(ti => `${ti.date.getUTCFullYear()}-0${ti.date.getUTCMonth() + 1}-${ti.date.getDate() - 1}` == today)
+                .filter(t1 => getDate(t1.date) == today)
         }));
 
 };
