cd37e4b75a1daff18af38a30b8a3bc516f561280
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-18 05:03:46 -0400

    Relocate ActivityList and create style sheet for it.

--- client/src/pages/indexPage/activityList/ActivityList.tsx
+++ client/src/pages/indexPage/activityList/ActivityList.tsx
@@ -1,13 +1,22 @@
 import { TodaysActivity } from "@/interfaces";
 import { displayMsInFractionalHourFormat } from "@/utils";
+import { useNavigate } from "react-router-dom";
+
+import styles from './ActivityList.module.css';
 
 interface props {
     activities: TodaysActivity[]
 }
+
 const ActivityList: React.FC<props> = ({ activities }) => {
+    const navigate = useNavigate();
     return <>
         {activities.map(activity => {
-            return <div>
+            return <div
+                className={styles.activity}
+                onClick={() => {
+                    navigate(`/task/${activity._id}`);
+                }}>
                 <span><>{activity.title} - {displayMsInFractionalHourFormat(activity.totalTimeToday)}</></span>
             </div>
         })}
