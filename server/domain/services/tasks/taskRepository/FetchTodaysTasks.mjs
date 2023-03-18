5ca1a316b1ba0182ebf915053f96b01f0a40d1d1
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-18 05:15:59 -0400

    Update FetchTodaysTasks to include total aggregated

--- server/domain/services/tasks/taskRepository/FetchTodaysTasks.mjs
+++ server/domain/services/tasks/taskRepository/FetchTodaysTasks.mjs
@@ -10,21 +10,27 @@ export const FetchTodaysTasks = async () => {
         '$where': `this.date.toJSON().slice(0, 10) == "${today}"`
     }
     ).sort('-_id');
-    return tasks
-        .map((task) => ({
-            _id: task?._id ?? 'stubAnId',
-            title: task?.title ?? 'no title',
-            date: task.date,
-            totalTimeToday: task.time
-                .filter(t1 => t1?.date)
-                .filter(t1 => getDate(t1.date) == today)
-                .map(t1 => t1.time)
-                .reduce((t1, t2) => t1 + t2, 0),
-            times: task.time
-                .filter(t1 => t1?.date)
-                .filter(t1 => getDate(t1.date) == today)
-        }));
+    const aggActivities = {
+        activities: tasks
+            .map((task) => ({
+                _id: task?._id ?? 'stubAnId',
+                title: task?.title ?? 'no title',
+                date: task.date,
+                totalTimeToday: task.time
+                    .filter(t1 => t1?.date)
+                    .filter(t1 => getDate(t1.date) == today)
+                    .map(t1 => t1.time)
+                    .reduce((t1, t2) => t1 + t2, 0),
+                times: task.time
+                    .filter(t1 => t1?.date)
+                    .filter(t1 => getDate(t1.date) == today)
+            }))
+    };
+    aggActivities.total = aggActivities.activities
+        .map(activity => activity.totalTimeToday)
+        .reduce((a1, a2) => a1 + a2, 0);
 
+    return aggActivities;
 };
 
 export default FetchTodaysTasks;
