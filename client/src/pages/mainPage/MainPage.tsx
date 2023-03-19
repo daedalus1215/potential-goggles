9a76671e2da01bf7dd4a681f4d00f7723ad02bc0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-19 16:31:36 -0400

    Moving buttons around MainPage

--- client/src/pages/mainPage/MainPage.tsx
+++ client/src/pages/mainPage/MainPage.tsx
@@ -2,31 +2,40 @@ import { Outlet, useNavigation, useLoaderData, useSubmit, Form } from 'react-rou
 import cn from 'classnames'
 import { Task } from '../../interfaces'
 import Sidebar from './sidebar/Sidebar'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import IconButton from '@/components/iconButton/IconButton'
 
 import styles from './MainPage.module.css';
 import { Category } from '@/components/button/Button'
+import { useSmallScreenSize } from '@/hooks/useSmallScreenSize'
+import TagButton from '@/components/tagButton/TagButton'
+import HomeButton from '@/components/homeButton/HomeButton'
 
 const MainPage = () => {
   const { tasks, q, selectedId } = useLoaderData() as { tasks: Task[]; q: string; selectedId: string }
   const navigation = useNavigation()
   const [isExpanded, setIsExpanded] = useState(true);
+  const isSmallScreen = useSmallScreenSize();
+  const dontShowOutlet = isSmallScreen && isExpanded;
 
   return (
     <>
-      <Sidebar tasks={tasks} q={q} selectedId={selectedId} isExpanded={isExpanded} />
+      <Sidebar classNames={cn({ [styles.smallScreenSidebar]: isSmallScreen })} tasks={tasks} q={q} selectedId={selectedId} isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
       <div id="detail" className={cn('detail', navigation.state === 'loading' ? 'loading' : '')}>
-        <IconButton
-        category={Category.info}
-          icon="bi bi-card-list"
-          onClick={() => setIsExpanded(!isExpanded)}
-        />
-        <Form method="post" className={styles.newForm}>
-          <input type="hidden" name="formId" value="newTask" />
-          <IconButton type="submit" icon="bi-plus-square-fill" category={Category.accent} />
-        </Form>
-        <Outlet />
+        {!dontShowOutlet && (<div className={styles.homeButtons}>
+          <IconButton
+            category={Category.info}
+            icon="bi bi-card-list"
+            onClick={() => setIsExpanded(!isExpanded)}
+          />
+          <HomeButton />
+          <Form method="post" className={styles.newForm}>
+            <input type="hidden" name="formId" value="newTask" />
+            <IconButton type="submit" icon="bi-plus-square-fill" category={Category.accent} />
+          </Form>
+          <TagButton />
+        </div>)}
+        {!dontShowOutlet && <Outlet />}
       </div>
     </>
   )
