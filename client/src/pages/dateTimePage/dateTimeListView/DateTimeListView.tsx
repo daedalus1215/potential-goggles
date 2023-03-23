2ac21ec7bfc005ac311d2107650e26326e9267f8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-22 20:51:00 -0400

    Add isExpanded styling to DateTimeListView

--- client/src/pages/dateTimePage/dateTimeListView/DateTimeListView.tsx
+++ client/src/pages/dateTimePage/dateTimeListView/DateTimeListView.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Task } from '@/interfaces';
 import DateTimeItem from './DateTimeItem';
 import styles from './DateTimeListView.module.css';
+import { ExpandedContext } from '@/contexts/ExpandedContext';
+import classNames from 'classnames';
 
 interface DateTimeListViewProp {
   task: Task
 }
 
 const DateTimeListView: React.FC<DateTimeListViewProp> = ({ task }) => {
+  const { isExpanded } = useContext(ExpandedContext);
+
   return (
-    <div className={styles.grid}>
+    <div className={classNames(styles.grid, { [styles.isExpanded]: isExpanded })}>
       {task.dateTimes?.map((dateTime) => <DateTimeItem
         dateTime={dateTime}
         taskId={task._id}
