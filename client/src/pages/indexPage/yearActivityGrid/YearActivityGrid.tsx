009d126856d88b7205fbdcc67dcd98d760f4f7a8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-26 18:38:51 -0400

    Filling in YearActivityGrid's content (spike)

--- client/src/pages/indexPage/yearActivityGrid/YearActivityGrid.tsx
+++ client/src/pages/indexPage/yearActivityGrid/YearActivityGrid.tsx
@@ -1,15 +1,42 @@
+import Tooltip from '@/components/toolTip/Tooltip';
+import classNames from 'classnames';
 import React from 'react';
 import styles from './YearActivityGrid.module.css';
 
 interface props {
     allActivities: any;
 }
+
 const YearActivityGrid: React.FC<props> = ({ allActivities }) => {
     console.log('allActivities', allActivities)
-    return <div className={styles.grid}>
-        {allActivities.map((index: number, activity: any) => (<div key={activity.date} className={styles.content}>
-            <span>{activity.date}</span>
+
+    const hover = (date:string, time:number, titles:string[]) => {
+        return (<div className={styles.hoverTitle}>
+            <div>Date: {date}</div>
+            <div>Hours: {(time / 1000 / 60 / 60).toFixed(4)}</div>
+            <ul >{titles.map((title: string) => <li>{title}</li>)}</ul>
         </div>)
+    }
+    return <div className={styles.grid}>
+        {allActivities.map((activity: any) => (
+            <Tooltip key={activity.date}
+                hover={hover(activity.date, activity.time, activity.titles)}>
+                <span className={classNames(styles.contentOutline, {
+                    [styles.brightGold]: (activity.time / 1000 / 60) > 160,
+                    [styles.gold]: (activity.time / 1000 / 60) < 160,
+                    [styles.lightGold]: (activity.time / 1000 / 60) < 140,
+                    [styles.darkestGreen]: (activity.time / 1000 / 60) < 120,
+                    [styles.darkGreen]: (activity.time / 1000 / 60) < 100,
+                    [styles.medGreen]: (activity.time / 1000 / 60) < 80,
+                    [styles.lightGreen]: (activity.time / 1000 / 60) < 60,
+                    [styles.yellow]: (activity.time / 1000 / 60) < 40,
+                    [styles.red]: activity.time < 20,
+
+                })}>
+
+                </span>
+            </Tooltip>
+        )
         )}
     </div>;
 }
