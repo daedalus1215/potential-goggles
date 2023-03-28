6f7459d58774a5870a18f3a996e636c52d67e04f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-03-27 20:14:17 -0400

    Adjusting styling on the datetime grid (color and width/size)

--- client/src/pages/indexPage/yearActivityGrid/YearActivityGrid.tsx
+++ client/src/pages/indexPage/yearActivityGrid/YearActivityGrid.tsx
@@ -10,34 +10,30 @@ interface props {
 const YearActivityGrid: React.FC<props> = ({ allActivities }) => {
     console.log('allActivities', allActivities)
 
-    const hover = (date:string, time:number, titles:string[]) => {
+    const hover = (date: string, time: number, titles: string[]) => {
         return (<div className={styles.hoverTitle}>
             <div>Date: {date}</div>
             <div>Hours: {(time / 1000 / 60 / 60).toFixed(4)}</div>
             <ul >{titles.map((title: string) => <li>{title}</li>)}</ul>
         </div>)
     }
-    return <div className={styles.grid}>
-        {allActivities.map((activity: any) => (
-            <Tooltip key={activity.date}
-                hover={hover(activity.date, activity.time, activity.titles)}>
-                <span className={classNames(styles.contentOutline, {
-                    [styles.brightGold]: (activity.time / 1000 / 60) > 160,
-                    [styles.gold]: (activity.time / 1000 / 60) < 160,
-                    [styles.lightGold]: (activity.time / 1000 / 60) < 140,
-                    [styles.darkestGreen]: (activity.time / 1000 / 60) < 120,
-                    [styles.darkGreen]: (activity.time / 1000 / 60) < 100,
-                    [styles.medGreen]: (activity.time / 1000 / 60) < 80,
-                    [styles.lightGreen]: (activity.time / 1000 / 60) < 60,
-                    [styles.yellow]: (activity.time / 1000 / 60) < 40,
-                    [styles.red]: activity.time < 20,
+    return <div className={styles.gridContainer}>
+        <div className={styles.grid}>
+            {allActivities.map((activity: any) => (
+                <Tooltip key={activity.date}
+                    hover={hover(activity.date, activity.time, activity.titles)}>
+                    <span className={classNames(styles.contentOutline, {
+                        [styles.darkestGreen]: (activity.time / 1000 / 60 /60) > 4,
+                        [styles.darkGreen]: (activity.time / 1000 / 60 /60) > 3 && (activity.time / 1000 / 60 /60) < 4,
+                        [styles.medGreen]: (activity.time / 1000 / 60 /60) > 2 && (activity.time / 1000 / 60 /60) < 3,
+                        [styles.red]: (activity.time / 1000 / 60 /60) > 1 && (activity.time / 1000 / 60 /60) < 3,
+                    })}>
 
-                })}>
-
-                </span>
-            </Tooltip>
-        )
-        )}
+                    </span>
+                </Tooltip>
+            )
+            )}
+        </div>
     </div>;
 }
 
