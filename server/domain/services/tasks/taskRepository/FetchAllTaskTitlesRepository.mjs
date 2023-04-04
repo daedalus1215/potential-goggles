5d4acea6ad66b965ac70f6ce73eb779369534c8e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-03 22:42:54 -0400

    Fix sort order for task titles

--- server/domain/services/tasks/taskRepository/FetchAllTaskTitlesRepository.mjs
+++ server/domain/services/tasks/taskRepository/FetchAllTaskTitlesRepository.mjs
@@ -1,7 +1,7 @@
 import TaskModel from "../../../../infrastructure/models/TaskModel.mjs";
 
 const FetchAllTaskTitlesRepository = async () => {
-    const tasks = await TaskModel.find().sort('-_id');
+    const tasks = await TaskModel.find().sort('-date');
     return tasks
         .map((task) => ({
             _id: task?._id ?? 'stubAnId',
