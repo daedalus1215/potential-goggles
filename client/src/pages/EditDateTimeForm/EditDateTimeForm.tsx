08169eeb50283670a802fa43cc3e902c55c7ccbd
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-15 20:48:40 -0400

    Add back button to Edit Date Time page

--- client/src/pages/EditDateTimeForm/EditDateTimeForm.tsx
+++ client/src/pages/EditDateTimeForm/EditDateTimeForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import cn from 'classnames';
-import { Button, SaveButton } from '../../components';
+import { Button, SaveButton, TopBar } from '@/components';
 import { DateTime, Task } from '../../interfaces';
 import { formatMinsAndSecsForDisplay } from '../../utils';
 import { minSecValidator, utcFormatValidator } from '../../utils/forms/validators';
@@ -8,6 +8,7 @@ import { Form, redirect, useLoaderData } from 'react-router-dom';
 
 import styles from './EditDateTimeForm.module.css';
 import { ExpandedContext } from '@/contexts/ExpandedContext';
+import BackButton from '@/components/BackButton';
 
 interface EditDateTimeFormProp {
   taskId: string,
@@ -16,7 +17,7 @@ interface EditDateTimeFormProp {
 
 const EditDateTimeForm: React.FC = () => {
   const { dateTime, taskId } = useLoaderData() as EditDateTimeFormProp;
-  const {isExpanded} = useContext(ExpandedContext);
+  const { isExpanded } = useContext(ExpandedContext);
   const minsAndSecs = formatMinsAndSecsForDisplay(dateTime.time);
 
   if (!dateTime) {
@@ -27,10 +28,18 @@ const EditDateTimeForm: React.FC = () => {
   }
 
   return (
+    <>
+          <TopBar>
+        <>
+          <BackButton path={`/date-time/${taskId}`} />
+        </>
+      </TopBar>
+
+
     <div className={styles.editDateTimeForm}>
       <Form
         method='post'
-        className={cn(styles.form, {[styles.notExpanded]: !isExpanded})}>
+        className={cn(styles.form, { [styles.notExpanded]: !isExpanded })}>
         <h2 className={styles.h2}>Edit Date Time</h2>
         <input hidden={true} name="taskId" value={taskId} readOnly />
         <input hidden={true} name="id" value={dateTime.id} readOnly />
@@ -39,6 +48,7 @@ const EditDateTimeForm: React.FC = () => {
         <SaveButton />
       </Form>
     </div>
+    </>
   );
 };
 
