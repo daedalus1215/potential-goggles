33444d71d378fe36e4fc0a2bafd94d843906f10d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-04-16 22:56:09 -0400

    Don't bother with : when entering time

--- server/application/requestHandlers/tasks/dateTime/updateDateTimeAction.mjs
+++ server/application/requestHandlers/tasks/dateTime/updateDateTimeAction.mjs
@@ -3,14 +3,10 @@ import TaskService from "../../../../domain/services/tasks/TaskService.mjs";
 export default async (req, res) => {
     const id = req.params.taskId;
     const dateTime = req.body;
-
-    if (dateTime.length > 5) {
-        // throw new Error('too many characters in time');
-        res.jsonp({ success: false, message: "Too many characters in time " });
-        return;
-    }
-    const pattern = new RegExp(/([0-9]\d|2[0-9]):([0-9]\d|2[0-9])/gs);
-    if (pattern.test(dateTime)) {
+    
+    console.log('dateTime', dateTime.time)
+    let isnum = /^\d+$/.test(dateTime.time);
+    if (!isnum) {
         res.jsonp({ success: false, message: "Required {minute:second} format, like: 00:00" });
         return;
     }
