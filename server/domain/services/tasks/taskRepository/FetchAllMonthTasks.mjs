4fd389b53d6de0c13c9d495426fc4f8d05236df3
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-20 23:20:28 -0400

    Create Fetch All Month Tasks Repo func

---
+++ server/domain/services/tasks/taskRepository/FetchAllMonthTasks.mjs
@@ -0,0 +1,42 @@
+import TaskModel from "../../../../infrastructure/models/TaskModel.mjs";
+import { getDate, getMonthDate } from '../../../../utils/getDate.mjs'
+
+export const fetchAllMonthTasks = async () => {
+    const tasks = await TaskModel.find();
+    const results = {};
+    tasks
+        .filter(task => task?.date)
+        .forEach(task => {
+            task.time
+                .filter(time => time?.date)
+                .map(time => {
+                    const theDate = getMonthDate(time.date);
+
+                    if (!results[theDate]) {
+                        results[theDate] = { time: 0, titles: [] };
+                    }
+
+                    results[theDate].time += time.time;
+
+                    const title = task?.title ?? 'no title';
+                    if (results[theDate]?.titles.indexOf(title) === -1) {
+                        results[theDate].titles.push(title);
+                    }
+
+                });
+        });
+    const newResults = [];
+    const keys = Object.keys(results);
+
+    for (let key of keys) {
+        newResults.push({
+            date: key,
+            ...results[key]
+        })
+    }
+
+    return newResults
+        .sort((res, res2) => new Date(res2.date) - new Date(res.date));
+};
+
+export default fetchAllMonthTasks;
