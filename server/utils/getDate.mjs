2ddfbe51850c7091fabf7e1180a3aa144af085df
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-20 23:21:35 -0400

    Add a helper function for getting mont dates

--- server/utils/getDate.mjs
+++ server/utils/getDate.mjs
@@ -10,3 +10,16 @@ export const getDate = (date) => {
     }
     return `${date.getUTCFullYear()}-${month}-${day}`;
 };
+
+export const getMonthDate = (date) => {
+    if (date === undefined) return null;
+    let month = date.getUTCMonth() + 1;
+    if (month < 10) {
+        month = `0${month}`
+    }
+    let day = date.getDate();
+    if (day < 10) {
+        day = `0${day}`
+    }
+    return `${date.getUTCFullYear()}-${month}`;
+};
