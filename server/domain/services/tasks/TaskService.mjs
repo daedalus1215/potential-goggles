5e8eaa20b042ed98ce00c4ff2e2658693ffc4a95
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-20 23:21:21 -0400

    Use Task for Repo instead of Repo in Action

--- server/domain/services/tasks/TaskService.mjs
+++ server/domain/services/tasks/TaskService.mjs
@@ -1,5 +1,6 @@
 import CreateDateTimeRepository from "./taskRepository/dateTime/CreateDateTimeRepository.mjs";
 import UpdateDateTimeRepository from "./taskRepository/dateTime/UpdateDateTimeRepository.mjs";
+import fetchAllMonthTasks from "./taskRepository/FetchAllMonthTasks.mjs";
 import FetchAllTasksRepository from "./taskRepository/FetchAllTasksRepository.mjs";
 import FetchAllTaskTitlesRepository from "./taskRepository/FetchAllTaskTitlesRepository.mjs";
 import FetchTaskByIdRepository from "./taskRepository/FetchTaskByIdRepository/FetchTaskByIdRepository.mjs";
@@ -7,6 +8,7 @@ import updateTask from "./taskRepository/updateTask/updateTask.mjs";
 
 export const TaskService = {
   fetchAllTasks: () => FetchAllTasksRepository(),
+  fetchAllMonthTasks: () => fetchAllMonthTasks(),
   fetchTaskById: (taskId) => FetchTaskByIdRepository(taskId),
   updateTask: (dto) => updateTask(dto),
   updateDateTimeOfTask: (taskId, dateTime) => UpdateDateTimeRepository(taskId, dateTime),
