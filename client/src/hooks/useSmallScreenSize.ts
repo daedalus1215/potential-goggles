f9913125e103ce9f0f7b89d88c1539040389f3e0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-08-21 22:05:32 -0400

    hotkey-saving: add default export for useSmallScreenSize

--- client/src/hooks/useSmallScreenSize.ts
+++ client/src/hooks/useSmallScreenSize.ts
@@ -4,3 +4,5 @@ export const useSmallScreenSize = () => {
     const windowSize = useRef([window.innerWidth, window.innerHeight]);
     return windowSize.current[0] < 500;
 };
+
+export default useSmallScreenSize;
