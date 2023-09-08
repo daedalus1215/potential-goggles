c924cd19efdc3fe06751ddf009d520048719a379
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-07 23:05:19 -0400

    Fixed getDate to remove end of month date, when it was the last day of the previous month.

--- server/domain/services/tasks/taskRepository/FetchAllDayTasks.mjs
+++ server/domain/services/tasks/taskRepository/FetchAllDayTasks.mjs
@@ -4,27 +4,23 @@ import { getDate } from '../../../../utils/getDate.mjs'
 export const FetchAllDayTasks = async () => {
     const tasks = await TaskModel.find();
     const results = {};
-    tasks
-        .filter(task => task?.date)
+    tasks.filter(task => task?.date)
         .forEach(task => {
             task.time
                 .filter(time => time?.date)
                 .map(time => {
                     const theDate = getDate(time.date);
-                    if (theDate === '2023-09-31') {
-                        console.log('theDate is 09 - 31', time.date)
-                    }
                     if (!results[theDate]) {
                         results[theDate] = { time: 0, titles: [] };
                     }
 
                     results[theDate].time += time.time;
 
+                    // because we do not need to add the title more than once
                     const title = task?.title ?? 'no title';
                     if (results[theDate]?.titles.indexOf(title) === -1) {
                         results[theDate].titles.push(title);
                     }
-
                 });
         });
     const newResults = [];
@@ -36,8 +32,6 @@ export const FetchAllDayTasks = async () => {
             ...results[key]
         })
     }
-
-
 
     return newResults
         .sort((res, res2) => new Date(res2.date) - new Date(res.date))
--- server/utils/__test__/getDate.test.mjs
+++ server/utils/__test__/getDate.test.mjs
@@ -37,6 +37,44 @@ describe('server/utils/__test__/getDate.test.mjs', () => {
             // Assert
             expect(actual).toEqual(expected);
         });
+        
+        // @TODO: What is going on in this scenario, because this is what is causing the bad date
+        it('should convert a UTC that says 09-01, but convert to it\'s actual value, 08-31', () => {
+            // Arrange
+            const august31 = '2023-09-01T03:05:48.202Z'
+            const date = new Date(august31);
+            const expected = '2023-08-31'; 
+
+            // Act
+            const actual = getDate(date);
+
+            // Assert
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return february on leap day', () => {
+            // Arrange
+            const date = new Date('2020-02-29');
+            const expected = '2020-02-29';
+
+            // Act
+            const actual = getDate(date);
+
+            // Assert
+            expect(actual).toEqual(expected);
+        });
+
+        it('should return march for day after leap day (not a real date for the month)', () => {
+            // Arrange
+            const date = new Date('2020-02-30');
+            const expected = '2020-02-29';
+
+            // Act
+            const actual = getDate(date);
+
+            // Assert
+            expect(actual).toEqual(expected);
+        });
     });
 
     describe('getMonthDate', () => {
