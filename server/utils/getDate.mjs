abdc81635036b7cf4aad1ba7e930b0e2f78496e0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-09 11:55:22 -0400

    Fix the dates on the backend

--- server/utils/getDate.mjs
+++ server/utils/getDate.mjs
@@ -1,26 +1,14 @@
+import { format } from 'date-fns';
+
 export const getDate = (date) => {
     if (date === undefined) return null;
-    let month = date.getMonth() + 1;
-    if (month < 10) {
-        month = `0${month}`
-    }
-    let day = date.getDate() + 1;
-    if (day < 10) {
-        day = `0${day}`
-    }
-    if (month == '02' && day === 30) {
-        day = date.getDate();
-    } else if (day > 31) {
-        day = 31
-    }
-    return `${date.getUTCFullYear()}-${month}-${day}`;
+    const dtDateOnly = new Date(date.valueOf() + date.getTimezoneOffset() * 60 * 1000);
+    return format(dtDateOnly, 'yyyy-MM-dd');
+    
 };
 
 export const getMonthDate = (date) => {
     if (date === undefined) return null;
-    let month = date.getUTCMonth() + 1;
-    if (month < 10) {
-        month = `0${month}`
-    }
-    return `${date.getUTCFullYear()}-${month}`;
+    
+    return format(date, 'yyyy-MM');
 };
