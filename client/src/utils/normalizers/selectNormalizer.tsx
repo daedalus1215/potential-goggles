74d137cc2bc0f1a6bae7e2c36fdb810d07c6810f
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-10 12:02:14 -0400

    Fix selectorNormalizer test suite and rename formatMinsAndSecsForDisplay test suite

--- client/src/utils/normalizers/selectNormalizer.tsx
+++ client/src/utils/normalizers/selectNormalizer.tsx
@@ -1,22 +1,29 @@
-interface EntityOption {
-  _id: string;
+export type EntityLabelOption = {
+  value: string | number;
+  label: string;
+}
+
+export type EntityNameOption = {
+  _id: string | number;
   name: string;
+}
+
+export type CombinedOptions = EntityNameOption | EntityLabelOption;
+
+
+type ReturnedOption = {
   value: string | number;
   label: string;
 }
 
-interface ReturnedOption {
-  value: string | number;
-  label: string;
-}
-
-const selectNormalizer = (options: EntityOption[]): ReturnedOption[] | null => {
-  return options instanceof Array
-    ? options?.map(d => ({
-        value: d?._id || d.value,
-        label: d?.name || d.label,
-      }))
-    : null;
+const selectNormalizer = (options: CombinedOptions[]): ReturnedOption[] | null => {
+  if (Array.isArray(options)) {
+    return options.map((d) => ({
+      value: (d as EntityNameOption)._id || (d as EntityLabelOption).value,
+      label: (d as EntityNameOption).name || (d as EntityLabelOption).label,
+    }));
+  }
+  return null;
 };
 
 export default selectNormalizer;
