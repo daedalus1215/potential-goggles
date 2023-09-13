092f2584e7770662a288b14f0b9209dca9d150b8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-13 15:29:08 -0400

    Remove moment, since we do not use now

--- server/utils/getDate.mjs
+++ server/utils/getDate.mjs
@@ -1,9 +1,5 @@
 import { format } from 'date-fns';
-import moment from 'moment-timezone';
 
-const myTimezone = 'America/New_York';
-const myDatetimeFormat = 'YYYY-MM-DD hh:mm:ss a';
-const myDatetimeFormat2 = 'YYYY-MM-DD';
 const dateRegExp = /\d{4}-\d{2}-\d{2}/;
 const monthRegExp = /\d{4}-\d{2}/;
 
@@ -27,22 +23,10 @@ export const formatMonth = (date) => {
     return format(date, 'yyyy-MM-dd').match(monthRegExp)[0];
 }
 
-export const getDateMoment = (date) => {
-    return moment(date)
-        .tz(myTimezone)
-        .format(myDatetimeFormat);
-};
-
 export const getMonthDate = (date) => {
     if (date === undefined) return null;
 
     return format(date, 'yyyy-MM');
-};
-
-export const getMonthDateMoment = (date) => {
-    if (date === undefined) return null;
-
-    return formatMonth(getMonthDate(date))
 };
 
 export const compareFormattedDate = (date, day) => {
