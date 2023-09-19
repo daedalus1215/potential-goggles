38d763886371e7dfa39543b032f4a16a218f2981
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-18 21:18:06 -0400

    object destructure in FetchTodaysTasksAction

--- server/application/requestHandlers/tasks/dateTime/FetchTodaysTasksAction.mjs
+++ server/application/requestHandlers/tasks/dateTime/FetchTodaysTasksAction.mjs
@@ -2,10 +2,7 @@ import FetchTodaysTasks from "../../../../domain/services/tasks/taskRepository/f
 
 
 export const FetchTodaysActivity = async (req, res) => {
-    console.log('req.query', req.query)
-    const date = req.query.date;
-    const tags = req.query.tags;
-    console.log('date0', date)
+    const { date, tags } = req.query;
     const updatedTaskWithDateTime = await FetchTodaysTasks(tags, date);
     res.jsonp(updatedTaskWithDateTime);
 };
