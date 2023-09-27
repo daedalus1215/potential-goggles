353daa136d10612727df6d0cd84a034e0922a576
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-26 23:02:00 -0400

    update DateTimeFixture and add ProjectFixture

--- client/src/dataFixtures/DateTimeFIxture.ts
+++ client/src/dataFixtures/DateTimeFIxture.ts
@@ -1,4 +1,4 @@
-export const dateTime = {
+export const DateTimeFixture = {
     id: 'dateTimeID',
     date: 'dateTimeDate',
     time: 'dateTimeTime',
---
+++ client/src/dataFixtures/ProjectFixture.ts
@@ -0,0 +1,17 @@
+export const ProjectFixture = [
+    {
+        key: 'project Id',
+        contract: 'contract for the project',
+        customer: 'customer for the contract'
+    },
+    {
+        key: 'project Id2',
+        contract: 'contract for the project2',
+        customer: 'customer for the contract2'
+    },
+    {
+        key: 'project Id3',
+        contract: 'contract for the project3',
+        customer: 'customer for the contract3'
+    }
+];
