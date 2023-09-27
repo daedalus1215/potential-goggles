bc2fbcd5782c7f808d7ad9d6bf98dcd9ac2182f2
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-26 23:02:23 -0400

    Fix formatTimeContractAndCustomer.test.ts

--- client/src/pages/indexPage/DownloadButton/__test__/formatTimeContractAndCustomer.test.ts
+++ client/src/pages/indexPage/DownloadButton/__test__/formatTimeContractAndCustomer.test.ts
@@ -1,43 +1,27 @@
+import { Task } from '@/interfaces';
 import formatTimeContractAndCustomer from '../formatTimeContractAndCustomer';
-import { displayMsInFractionalHourFormat } from 'utils';
+import { displayMsInFractionalHourFormat } from '@/utils';
+import { TaskFixture } from '@/dataFixtures/TaskFixture';
+import { ProjectFixture } from '@/dataFixtures/ProjectFixture';
 
 describe('src/components/TaskListView/__test__/formatTimeContractAndCustomer.test.js', () => {
   describe('formatTimeContractAndCustomer', () => {
-    const projects = [
-      {
-        key: 'project Id',
-        contract: 'contract for the project',
-        customer: 'customer for the contract'
-      },
-      {
-        key: 'project Id2',
-        contract: 'contract for the project2',
-        customer: 'customer for the contract2'
-      },
-      {
-        key: 'project Id3',
-        contract: 'contract for the project3',
-        customer: 'customer for the contract3'
-      }
-    ];
-    const task = {
-      time: 10000,
-      contractId: 'project Id3',
-      description: 'task description becomes file name'
-    };
-
     it('should return a formatted task in preperation of saving into the json file', () => {
+      // Arrange
       const expectedTaskWithProject = {
-        ...task,
-        time: displayMsInFractionalHourFormat(task.time),
-        contract: projects[2].contract,
-        customer: projects[2].customer
+        ...TaskFixture,
+        time: displayMsInFractionalHourFormat(TaskFixture.time),
+        // contract: ProjectFixture[2].contract,
+        // customer: ProjectFixture[2].customer
       };
 
+      // Act
       const actualTaskWithProject = formatTimeContractAndCustomer(
-        task,
-        projects
+        TaskFixture,
+        ProjectFixture
       );
+
+      // Assert
       expect(actualTaskWithProject).toEqual(expectedTaskWithProject);
     });
   });
