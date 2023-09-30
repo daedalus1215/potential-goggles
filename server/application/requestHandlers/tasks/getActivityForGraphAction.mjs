ee072bfc8e11f67e5b0bd6854d85bc64e65379f8
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-09-30 14:21:46 -0400

    spike Setup getActivityForGraphAction and bring in faker

---
+++ server/application/requestHandlers/tasks/getActivityForGraphAction.mjs
@@ -0,0 +1,49 @@
+import { faker } from '@faker-js/faker';
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const options = {
+    plugins: {
+        title: {
+            display: true,
+            text: 'Chart.js Bar Chart - Stacked',
+        },
+    },
+    responsive: true,
+    scales: {
+        x: {
+            stacked: true,
+        },
+        y: {
+            stacked: true,
+        },
+    },
+};
+
+const getActivityForGraphAction = (req, res) => {
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+                backgroundColor: 'rgb(255, 99, 132)',
+            },
+            {
+                label: 'Dataset 2',
+                data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+                backgroundColor: 'rgb(75, 192, 192)',
+            },
+            {
+                label: 'Dataset 3',
+                data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+                backgroundColor: 'rgb(53, 162, 235)',
+            },
+        ],
+    };
+
+    res.jsonp({data, options});
+};
+
+export default getActivityForGraphAction;
