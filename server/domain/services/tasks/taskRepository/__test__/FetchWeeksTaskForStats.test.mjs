1c685a7969c51f5d804d86427c59434da35d9f9d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-10-02 22:14:43 -0400

    Get UT setup for FetchWeeksTaskForStats

--- server/domain/services/tasks/taskRepository/__test__/FetchWeeksTaskForStats.test.mjs
+++ server/domain/services/tasks/taskRepository/__test__/FetchWeeksTaskForStats.test.mjs
@@ -1,27 +1,52 @@
+import TaskModel from "../../../../../infrastructure/models/TaskModel";
+
 describe('server/domain/services/tasks/taskRepository/__test__/FetchWeeksTaskForStats.test.mjs', () => {
     describe('FetchWeeksTaskForStats', () => {
+        beforeEach(() => {
+            jest.mock("../../../../../infrastructure/models/TaskModel");
+        });
         it('should...', () => {
             // Arrange
             const expected = {
                 data: {
                     labels: [
-                        "2023-09-26", 
+                        "2023-09-26",
                         "2023-09-25",
-                        "2023-09-24", 
-                        "2023-09-23", 
-                        "2023-09-22", 
-                        "2023-09-21", 
+                        "2023-09-24",
+                        "2023-09-23",
+                        "2023-09-22",
+                        "2023-09-21",
                         "2023-09-20"
                     ],
                     datasets: [
                         {
                             label: "Dishes",
-                            data: []
+                            data: [4200000, 0, 0, 0, 0, 0, 600000],
+                            backgroundColor: 'rgb(197, 125, 206)'
+                        },
+                        {
+                            label: "Misc",
+                            data: [0, 0, 4200000, 0, 0, 0, 0],
+                            backgroundColor: 'rgb(202, 249, 211)'
                         }
                     ]
                 },
                 options: {
-
+                    "plugins": {
+                        "title": {
+                            "display": true,
+                            "text": "Chart.js Bar Chart - Stacked"
+                        }
+                    },
+                    "responsive": true,
+                    "scales": {
+                        "x": {
+                            "stacked": true
+                        },
+                        "y": {
+                            "stacked": true
+                        }
+                    }
                 }
             };
             const bothTimesWillBePickedUp = {
@@ -29,7 +54,7 @@ describe('server/domain/services/tasks/taskRepository/__test__/FetchWeeksTaskFor
                 tags: [
                     "asdsdasdsa"
                 ],
-                time: [ 
+                time: [
                     {
                         _id: "64decee8de58d7e77212f4cc",
                         date: "2023-09-26T03:38:45.610Z",
@@ -106,8 +131,10 @@ describe('server/domain/services/tasks/taskRepository/__test__/FetchWeeksTaskFor
                 ...willFindTasks,
                 noTimeWIllbePickedUp
             ];
+            TaskModel.find = jest.fn().mockImplementation(() => tasks);
+
             // Act
-            const actual = FetchWeeksTaskForStats(tasks);
+            const actual = FetchWeeksTaskForStats(new Date("2023-09-26T00:00:00.000Z"));
 
             // Assert
             expect(actual).toEqual(expected)
