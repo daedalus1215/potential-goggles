12bbea8075db6aa5ef579a1d7b674d805dba76d5
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-10-04 23:05:27 -0400

    Update TaskFixture so it sets dates

--- server/dataFixtures/TaskFixture.mjs
+++ server/dataFixtures/TaskFixture.mjs
@@ -6,18 +6,19 @@ export const dishesTask = {
     time: [
         {
             _id: "64decee8de58d7e77212f4cc",
-            date: "2023-09-26T03:38:45.610Z",
-            time: 4200000 //@TODO: Left off here, we need to borrow millisToMinutesAndSeconds and 
+            date: new Date("2023-09-26T12:38:45.000Z"),
+            time: 4200001 //@TODO: Left off here, we need to borrow millisToMinutesAndSeconds and 
         },
         {
             _id: "64e816b5de58d7e77212fdfe",
-            date: "2023-09-20T02:59:08.155Z",
-            time: 600000
+            date: new Date("2023-09-20T12:59:08.000Z"),
+            time: 600001
         },
     ],
     contractId: 0,
+    title: 'Dishes',
     description: "<p>Dishes</p>\n<p>some paragraphs</p>",
-    date: "2023-10-01T14:50:23.608Z"
+    date: new Date("2023-10-01T14:50:23.608Z")
 }
 
 export const miscTask = {
@@ -28,21 +29,27 @@ export const miscTask = {
     time: [
         {
             _id: "64decee8de58d7e77212f4cc",
-            date: "2023-09-12T11:33:12.362Z",
+            date: new Date("2023-09-12T11:33:12.362Z"),
             time: 4200000
         },
         {
             _id: "64e816b5de58d7e77212fdfe",
-            date: "2023-09-11T11:33:12.362Z",
+            date: new Date("2023-09-11T11:33:12.362Z"),
             time: 600000
         },
         {
             _id: "64decee8de58d7e77212f4cc",
-            date: "2023-09-24T03:38:45.610Z",
-            time: 4200000
+            date: new Date("2023-09-24T12:38:45.000Z"),
+            time: 4200002
+        },
+        {
+            _id: "64decee8de58d7e77212f4cc",
+            date: new Date("2023-09-24T12:38:45.000Z"),
+            time: 12
         },
     ],
     contractId: 0,
+    title: 'Misc',
     description: "<p>Misc</p>\n<p>some paragraphs</p>",
     date: "2023-09-12T11:33:12.362Z"
 };
@@ -55,21 +62,22 @@ export const hiddenTask = {
     time: [
         {
             _id: "64decee8de58d7e77212f4cc",
-            date: "2023-09-12T11:33:12.362Z",
+            date: new Date("2023-09-12T11:33:12.362Z"),
             time: 4200000
         },
         {
             _id: "64e816b5de58d7e77212fdfe",
-            date: "2023-09-11T11:33:12.362Z",
+            date: new Date("2023-09-11T11:33:12.362Z"),
             time: 600000
         },
         {
             _id: "64decee8de58d7e77212f4cc",
-            date: "2023-09-19T03:38:45.610Z",
+            date: new Date("2023-09-19T03:38:45.610Z"),
             time: 4200000
         },
     ],
     contractId: 0,
+    title: 'Not Gonna Find Us',
     description: "<p>Not Gonna Find Us</p>\n<p>some paragraphs</p>",
-    date: "2023-09-12T11:33:12.362Z"
+    date: new Date("2023-09-12T11:33:12.362Z")
 };
