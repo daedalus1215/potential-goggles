c4b402aa4b1c470f24778968d5c4b0b68982ce61
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-10-09 21:12:15 -0400

    apply new func to Stack Action

--- server/application/requestHandlers/tasks/getStatsForStackForRangeOfDatesAction.mjs
+++ server/application/requestHandlers/tasks/getStatsForStackForRangeOfDatesAction.mjs
@@ -1,9 +1,9 @@
 import TaskService from '../../../domain/services/tasks/TaskService.mjs';
+import { parseArrayString } from '../../../utils/parseArrayString.mjs';
 
 const getStatsForStackForRangeOfDatesAction = async (req, res) => {
     const { date, days, includeTags, excludeTags } = req.query;
-    console.log('includeTags',includeTags)
-    const data = await TaskService.fetchStackGraph(new Date(date), days, { includeTags, excludeTags });
+    const data = await TaskService.fetchStackGraph(new Date(date), days, { includeTags: parseArrayString(includeTags), excludeTags });
     res.jsonp(data);
 };
 
