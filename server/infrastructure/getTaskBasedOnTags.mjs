ee40639656d764643506b20194245619d1122b61
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-10-10 22:02:32 -0400

    Remove console logs

--- server/infrastructure/getTaskBasedOnTags.mjs
+++ server/infrastructure/getTaskBasedOnTags.mjs
@@ -7,12 +7,9 @@ import TaskModel from "./models/TaskModel.mjs";
 export const getTaskBasedOnTags = async (predicates) => {
     const { includeTags, excludeTags } = predicates;
     if (includeTags) {
-        console.log('had included tags')
         return await TaskModel.find({ tags: { $in: includeTags } });
     } else if (excludeTags) {
-        console.log('had excluded tags')
         return await TaskModel.find({ tags: { $nin: excludeTags } });
     }
-    console.log('no tags');
     return await TaskModel.find();
 };
