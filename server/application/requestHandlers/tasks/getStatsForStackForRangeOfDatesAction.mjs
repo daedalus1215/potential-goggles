ee1bff9eafd3fe2ff522b48f3886089dc90cad9e
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-10-10 21:58:52 -0400

    fix getStatsForStackForRangeOfDatesAction

--- server/application/requestHandlers/tasks/getStatsForStackForRangeOfDatesAction.mjs
+++ server/application/requestHandlers/tasks/getStatsForStackForRangeOfDatesAction.mjs
@@ -3,7 +3,13 @@ import { parseArrayString } from '../../../utils/parseArrayString.mjs';
 
 const getStatsForStackForRangeOfDatesAction = async (req, res) => {
     const { date, days, includeTags, excludeTags } = req.query;
-    const data = await TaskService.fetchStackGraph(new Date(date), days, { includeTags: parseArrayString(includeTags), excludeTags });
+    const data = await TaskService.fetchStackGraph(
+        new Date(date), 
+        days, 
+        { 
+            includeTags: includeTags ? parseArrayString(includeTags) : null, 
+            excludeTags: excludeTags ? parseArrayString(excludeTags) : null 
+        });
     res.jsonp(data);
 };
 
