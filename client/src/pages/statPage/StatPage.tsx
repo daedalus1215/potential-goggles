23389a045bcabc3103bc7e0fe9ab6d521e23aa4c
lala <13687739+daedalus1215@users.noreply.github.com>
2023-10-14 13:40:39 -0400

    Revert "Stackgraph9"

--- client/src/pages/statPage/StatPage.tsx
+++ client/src/pages/statPage/StatPage.tsx
@@ -8,17 +8,14 @@ import {
     Title,
     Tooltip,
     Legend,
-    ChartOptions,
-    ChartData,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { useLoaderData } from 'react-router-dom';
 
 const StatPage: React.FC = () => {
-    const remoteData = useLoaderData() as any;
+    const remoteData = useLoaderData() as unknown;
     return <>
-        {remoteData?.weekStack?.data && <Bar options={remoteData.weekStack.options} data={remoteData.weekStack.data} />}
-        {remoteData?.monthStack?.data && <Bar options={remoteData.monthStack.options} data={remoteData.monthStack.data} />}
+        {remoteData && <Bar options={remoteData.options} data={remoteData.data} />}
     </>
 }
 
