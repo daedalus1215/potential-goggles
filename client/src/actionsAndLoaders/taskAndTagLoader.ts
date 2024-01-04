43cdca6a8185102ae11cd1cb3edc178e62e2c397
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-01-03 23:21:08 -0500

    remove comments

--- client/src/actionsAndLoaders/taskAndTagLoader.ts
+++ client/src/actionsAndLoaders/taskAndTagLoader.ts
@@ -2,15 +2,12 @@ import { LoaderFunctionArgs } from "react-router-dom";
 import { fetchTags, fetchTask } from "./loaders";
 
 export const taskAndTagLoader = async ({ params }: LoaderFunctionArgs) => {
-    console.log('1')
     const task = await fetchTask(params?.taskId ?? '')
-    console.log('2')
     const tags = await fetchTags();
-    console.log('3')
 
     const hash: any = {};
     task.tags.forEach(tag => hash[tag] = true);
-    console.log('hash', hash)
+
     const options = tags.map(tag => {
         const option = { ...tag, selected: false };
         if (hash[tag.name]) {
@@ -18,5 +15,6 @@ export const taskAndTagLoader = async ({ params }: LoaderFunctionArgs) => {
         }
         return option;
     });
+    
     return { task, options }
 }
