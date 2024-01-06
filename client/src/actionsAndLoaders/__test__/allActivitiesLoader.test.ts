735e858a784a296ef3811a94097d67eebf124e66
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-01-05 22:17:53 -0500

    Update createRequest

--- client/src/actionsAndLoaders/__test__/allActivitiesLoader.test.ts
+++ client/src/actionsAndLoaders/__test__/allActivitiesLoader.test.ts
@@ -1,15 +1,64 @@
-describe('allActivitiesLoader', () => {
-    it('should...', () => {
-        // Arrange
-        const request = {
-            url: 'a url'
-        };
-        const expected = {};
-        
-        // Act
-        const actual = allActivitiesLoader((({ request: { URL: api } }) as unknown as LoaderFunctionArgs))
+import { LoaderFunctionArgs } from "react-router-dom";
+import { allActivitiesLoader } from "../allActivitiesLoader";
+import { api } from '@/config.json';
+import { createRequest } from "@/testUtils/createRequest";
 
-        // Assert
-        expect(actual).toEqual(expected);
+import {
+    fetchAllDayTasks,
+    fetchTodaysActivities,
+    fetchAllMonthTasksAction,
+    fetchTags
+} from "../loaders";
+import { AggregateActivity } from "@/interfaces";
+import { createMock } from "@/testUtils/createMock";
+
+jest.mock('../loaders', () => ({
+    fetchAllDayTasks: jest.fn(),
+    fetchTodaysActivities: jest.fn(),
+    fetchAllMonthTasksAction: jest.fn(),
+    fetchTags: jest.fn(),
+}));
+
+describe('allActivitiesLoader', () => {
+    describe('with no arguments', () => {
+        it('should ', async () => {
+            // Arrange
+            const request = createRequest();
+            const allActivities: AggregateActivity = {
+                activities: [],
+                total: 0
+            };
+            const todaysActivities: AggregateActivity = {
+                activities: [],
+                total: 2
+            };
+            const expectedAllMonthTasks: AggregateActivity = {
+                activities: [],
+                total: 3
+            };
+            const tags = [{ id: 'tagId' }];
+            const expected = {
+                allActivities: allActivities,
+                monthActivities: expectedAllMonthTasks,
+                options: [undefined,],
+                queryDate: null,
+                queryExcludeTags: null,
+                queryIncludeTags: null,
+                tags,
+                todaysActivities: todaysActivities,
+            };
+            createMock(fetchAllDayTasks, allActivities);
+            createMock(fetchTodaysActivities, todaysActivities);
+            createMock(fetchAllMonthTasksAction, expectedAllMonthTasks);
+            createMock(fetchTags, tags);
+    
+            // Act
+            const actual = await allActivitiesLoader(request as unknown as LoaderFunctionArgs);
+    
+            // Assert
+            expect(actual).toEqual(expected);
+        });
     });
+
+    //@TODO: Do the others with args
 });
