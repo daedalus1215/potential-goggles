b1a8b37cce31a39e376d638027b15ef55b663177
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-03-06 09:37:51 -0500

    Handle HttpError accordingly in getTaskByIdAction

--- server/application/requestHandlers/tasks/getTaskByIdAction.mjs
+++ server/application/requestHandlers/tasks/getTaskByIdAction.mjs
@@ -1,10 +1,14 @@
 import TaskService from "../../../domain/services/tasks/TaskService.mjs";
+import HttpError from "../../constants/HttpError.mjs";
 
-const getTaskByIdAction = async (req, res) => {
+const getTaskByIdAction = async (req, res, next) => {
   if (!req?.params?.id) {
     return res.jsonp({ error: 'Need Id' });
   }
   const task = await TaskService.fetchTaskById(req.params.id);
+  if (task instanceof HttpError) {
+    return next(task);
+  }
   res.jsonp(task);
 };
 
